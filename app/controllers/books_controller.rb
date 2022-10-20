6335dd5e6727d77ea49510adc33ff2e9070ee00c
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-10-20 13:56:21 +0000

    [Updata] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
 
   def show
     @book=Book.find(params[:id])
+    @books=Book.all
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2 }
   has_one_attached :profile_image
 
+  validates :title, presence: true
+  validates :body, presence: true
+
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/default-image.jpeg')
+      file_path = Rails.root.join('app/assets/images/no-image.jpeg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
